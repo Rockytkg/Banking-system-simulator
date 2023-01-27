675a5cca72c0ca47a2ee4c3e47a63fb96af0c1de
囧 <2696916846@qq.com>
2023-01-27 17:28:05 +0800

    新增加密功能，让文件加密储存

--- init.c
+++ init.c
@@ -3,9 +3,11 @@
 //名为 head 的全局变量，该变量将指向链表的第一个账户
 BankAccount *head = NULL;
 BankAccount *currents = NULL;
+unsigned char key[16];
 
 //打印当前链表中自定义用户开始以后的的所有数据
 void printBankAccount(BankAccount *account) {
+    printf("\033[32m提示\033[0m\033[1m：当前系统中用户有：\n");
     while (account != NULL) {
         printf("账号: %s\n", account->accountNumber);
         printf("户主: %s\n", account->accountHolderName);
@@ -17,11 +19,12 @@ void printBankAccount(BankAccount *account) {
         if (trans == NULL)
             printf("没有操作记录！！！\n");
         while (trans != NULL) {
-            printf("\t%s\n", trans->message);
+            printf("%s\n", trans->message);
             trans = trans->next;
         }
         account = account->next;
     }
+    countdown(120);
 }
 
 //校验函数
@@ -29,7 +32,9 @@ void open_file(FILE *fp) {
     if (fp == NULL) {
         system("cls");
         printf("\033[1;31m警告：\033[0m\n\n");
-        printf("\033[1m程序异常，打开文件时出错！！！");
+        printf("\033[1m程序异常，打开文件时出错！！！\n");
+        printf("\033[1m请删除所有数据文件后重试。");
+        clear_kb_buffer();
         getchar();
         exit(0);
     }
@@ -53,9 +58,11 @@ void saveBankAccounts() {
         current = current->next;
     }
     fclose(file);
+    encrypt_file();
 }
 
 void loadBankAccounts() {
+    decrypt_file();
     FILE *file = fopen("bankAccounts.txt", "r");
     open_file(file);
     BankAccount *current = NULL;
@@ -126,12 +133,13 @@ void loadBankAccounts() {
         current->next = NULL;
     free(line);
     fclose(file);
-    //printBankAccount(head);
+    remove("bankAccounts.txt");
+    printBankAccount(head);
 }
 
 //初始化用户数据
 int DocumentVerification() {
-    if (fopen("bankAccounts.txt", "r") == NULL) {
+    if (fopen("AESBankAccounts.txt", "r") == NULL) {
         createAccount("1234567890", "马宏泰", "麻花银行", "123456");
         createAccount("2234567890", "马泰", "麻花银行", "123456");
         createAccount("3234567890", "泰宏", "麻花银行", "123456");
@@ -139,4 +147,78 @@ int DocumentVerification() {
         return 0;
     }
     return 1;
+}
+
+// 加密文件
+void encrypt_file() {
+    // 打开文件
+    FILE *fin = fopen("bankAccounts.txt", "rb");
+    FILE *fout = fopen("AESBankAccounts.txt", "wb");
+    open_file(fin);
+    open_file(fout);
+    // 初始化AES加密器
+    EVP_CIPHER_CTX *ctx;
+    ctx = EVP_CIPHER_CTX_new();
+    EVP_EncryptInit(ctx, EVP_aes_128_cbc(), key, NULL);
+    // 加密文件
+    unsigned char buffer[1024];
+    int bytes_read;
+    int len;
+    while ((bytes_read = fread(buffer, 1, sizeof(buffer), fin)) > 0) {
+        EVP_EncryptUpdate(ctx, buffer, &len, buffer, bytes_read);
+        fwrite(buffer, 1, len, fout);
+    }
+    EVP_EncryptFinal(ctx, buffer, &len);
+    fwrite(buffer, 1, len, fout);
+    // 关闭文件
+    fclose(fin);
+    fclose(fout);
+    EVP_CIPHER_CTX_free(ctx);
+    remove("bankAccounts.txt");
+}
+
+// 解密文件
+void decrypt_file() {
+    // 打开文件
+    FILE *fin = fopen("AESBankAccounts.txt", "rb");
+    FILE *fout = fopen("bankAccounts.txt", "wb");
+    open_file(fin);
+    open_file(fout);
+    // 初始化AES解密器
+    EVP_CIPHER_CTX *ctx;
+    ctx = EVP_CIPHER_CTX_new();
+    EVP_DecryptInit(ctx, EVP_aes_128_cbc(), key, NULL);
+    // 解密文件
+    unsigned char buffer[1024];
+    int bytes_read;
+    int len;
+    while ((bytes_read = fread(buffer, 1, sizeof(buffer), fin)) > 0) {
+        EVP_DecryptUpdate(ctx, buffer, &len, buffer, bytes_read);
+        fwrite(buffer, 1, len, fout);
+    }
+    EVP_DecryptFinal(ctx, buffer, &len);
+    fwrite(buffer, 1, len, fout);
+    // 关闭文件
+    fclose(fin);
+    fclose(fout);
+    EVP_CIPHER_CTX_free(ctx);
+    remove("AESBankAccounts.txt");
+}
+
+void generateKey() {
+    FILE *fa = fopen("AESBankAccounts.txt", "rb");
+    if (fa) {
+        FILE *fp = fopen("key.bin", "rb");
+        // key.bin文件不存在，抛出异常
+        open_file(fp);
+        // key.bin文件存在，从文件中读取密钥
+        fread(key, 1, 16, fp);
+        fclose(fp);
+    } else {
+        // AESBankAccounts.txt 和 key.bin文件不存在，生成随机密钥
+        RAND_bytes(key, 16);
+        FILE *fp = fopen("key.bin", "wb");
+        fwrite(key, 1, 16, fp);
+        fclose(fp);
+    }
 }
